f7adcd910ead7c097db09eefd281be0bc2a0350b
YOLO <phuong.nguyendang@hikergames.com>
2019-04-23 17:34:33 +0700

    thêm Generate platform Manager

---
+++ Assets/Scripts/GeneratePlatformManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Amanotes.Data;
+using UnityEngine;
+
+
+public enum ActionEventType
+{
+    TURNLEFT,
+    TURNRIGHT,
+    JUMPUP,
+    JUMPOVER,
+    FALLDOWN
+}
+
+
+public class GeneratePlatformManager : MonoBehaviour
+{
+    private List<ActionEvent> actions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void GenerateActionData(List<NoteData> noteDatas)
+    {
+        actions = new List<ActionEvent>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class ActionEvent
+{
+    public ActionEventType actionEventType;
+    public int index;
+}
