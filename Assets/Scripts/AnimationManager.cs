22bc5cff9c686c691250a9c3a07104341de6de0f
YOLO <phuong.nguyendang@hikergames.com>
2019-05-29 15:49:04 +0700

    Them aniamtion them test scene animation

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform cube;
+
+    private int jumptriggerhash = 0;
+
+    private int speedhash = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumptriggerhash = Animator.StringToHash("jump");
+        speedhash = Animator.StringToHash("speed");
+
+        animator.SetFloat(speedhash, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger(jumptriggerhash);
+
+            animator.MatchTarget(cube.position, cube.rotation, AvatarTarget.RightHand,
+                new MatchTargetWeightMask(Vector3.one, 1f), 0.41f, 0.58f);
+        }
+    }
+}
